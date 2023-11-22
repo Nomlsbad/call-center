f366593f490f3b550fedef8ca3b77c89d9e615c4
Yurasov Ilia <iliayurasov@yandex.ru>
2023-11-22 14:16:00 +0300

    Add base operator functional (#4)

---
+++ app/include/Operator.h
@@ -0,0 +1,31 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+#include <thread>
+
+class Operator
+{
+public:
+    using IdType = unsigned long long;
+
+public:
+    Operator(): id(nextId++) {};
+
+    void acceptCall(class CallDetailRecord& callDetail);
+
+private:
+
+    void talk();
+
+private:
+    IdType id = 0;
+
+    bool isBusy = false;
+
+    std::thread callThread;
+
+    static IdType nextId;
+};
+
+
+#endif //OPERATOR_H
