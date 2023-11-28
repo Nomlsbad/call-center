091b0ed9bfbaa67ab18ae1d747729e7446f6f3ae
Nomlsbad <iliayurasov@yandex.ru>
2023-11-28 16:06:50 +0300

    Add logging to operator

--- app/src/Operator.cpp
+++ app/src/Operator.cpp
@@ -2,10 +2,20 @@
 
 IdType Operator::nextId = 1;
 
+Operator::Operator()
+    : id(nextId++),
+      operatorLogger(Log::Logger::getInstance(LOG4CPLUS_TEXT("OperatorLogger"))){};
+
 void Operator::acceptCall(IdType callId)
 {
-    if (!isConnected()) return;;
+    if (!isConnected())
+    {
+        LOG4CPLUS_WARN(operatorLogger,
+                       "Operator: Operator[" << id << "]: this operator doesn't connect to call center");
+        return;
+    }
 
+    LOG4CPLUS_WARN(operatorLogger, "Operator: Operator[" << id << "]: operator was accepted call");
     callThread = std::thread(&Operator::talk, this, callId);
     callThread.detach();
 }
@@ -13,8 +23,12 @@ void Operator::acceptCall(IdType callId)
 void Operator::talk(IdType callId) const
 {
     if (!isConnected()) return;
+    LOG4CPLUS_WARN(operatorLogger, "Operator: Operator[" << id << "]: talking was began");
 
     callCenter->responseCall(callId, id, boost::posix_time::microsec_clock::local_time());
     std::this_thread::sleep_for(std::chrono::seconds(5));
     callCenter->endCall(callId, CallEndingStatus::OK, boost::posix_time::microsec_clock::local_time());
+
+
+    LOG4CPLUS_WARN(operatorLogger, "Operator: Operator[" << id << "]: talking was ended");
 }
