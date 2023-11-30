06cbe48418a407c0d076f9eaa4a1d52f36b1b9b0
Nomlsbad <iliayurasov@yandex.ru>
2023-11-30 13:24:22 +0300

    Add run and doRead methods with empty onRead

--- app/include/server/HttpSession.h
+++ app/include/server/HttpSession.h
@@ -9,6 +9,7 @@
 
 namespace beast = boost::beast;
 namespace http = beast::http;
+namespace net = boost::asio;
 
 class HttpSession : public std::enable_shared_from_this<HttpSession>
 {
@@ -16,14 +17,22 @@ public:
 
     explicit HttpSession(tcp::socket&& socket);
 
-    void run(){};
+    void run();
+
+private:
+
+    void doRead();
+    void onRead(beast::error_code errorCode, size_t bytes_transfered);
 
 private:
 
     beast::tcp_stream stream;
+    beast::flat_buffer buffer;
 
     static constexpr size_t queueSize = 8;
     std::vector<http::message_generator> responceQueue;
+
+    std::optional<http::request_parser<http::string_body>> parser;
 };
 
 #endif // HTTPSESSION_H
--- app/src/server/HttpSession.cpp
+++ app/src/server/HttpSession.cpp
@@ -5,3 +5,22 @@ HttpSession::HttpSession(tcp::socket&& socket)
 {
     responceQueue.reserve(queueSize);
 }
+
+void HttpSession::run()
+{
+    net::dispatch(stream.get_executor(), beast::bind_front_handler(&HttpSession::doRead, shared_from_this()));
+}
+
+void HttpSession::doRead()
+{
+    parser.emplace();
+    parser->body_limit(20);
+
+    stream.expires_after(std::chrono::seconds(30));
+
+    http::async_read(stream, buffer, *parser, beast::bind_front_handler(&HttpSession::onRead, shared_from_this()));
+}
+
+void HttpSession::onRead(beast::error_code errorCode, size_t bytes_transfered)
+{
+}
