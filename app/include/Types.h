b2f438e7f91e909bebe8ba8dc1398d5932d5cffc
Nomlsbad <iliayurasov@yandex.ru>
2023-12-01 19:37:51 +0300

    Update types

--- app/include/Types.h
+++ app/include/Types.h
@@ -1,9 +1,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
-
 
 using IdType = unsigned long long;
 using Date = boost::posix_time::ptime;
@@ -11,5 +10,8 @@ using TimeDuration = boost::posix_time::time_duration;
 
 using tcp = boost::asio::ip::tcp;
 
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
 
 #endif //TYPES_H
