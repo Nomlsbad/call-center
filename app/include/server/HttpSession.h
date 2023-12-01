d7862d6e64b493facb6117ce480d34d96dee3763
Nomlsbad <iliayurasov@yandex.ru>
2023-12-01 19:45:46 +0300

    Add requests handling to HttpSession

--- app/include/server/HttpSession.h
+++ app/include/server/HttpSession.h
@@ -3,34 +3,40 @@
 
 #include "Types.h"
 
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
+#include <deque>
 #include <memory>
 
-namespace beast = boost::beast;
-namespace http = beast::http;
-namespace net = boost::asio;
+class CallController;
 
 class HttpSession : public std::enable_shared_from_this<HttpSession>
 {
 public:
 
-    explicit HttpSession(tcp::socket&& socket);
+    explicit HttpSession(tcp::socket&& socket, std::weak_ptr<CallController> controller);
 
     void run();
 
 private:
 
-    void doRead();
+    void read();
     void onRead(beast::error_code errorCode, size_t bytes_transfered);
+
+    bool write();
+    void onWrite(bool keepAlive, beast::error_code errorCode, size_t bytes_transfered);
+
+    void addResponseToQueue(http::message_generator response);
+
+    void close();
 
 private:
 
     beast::tcp_stream stream;
     beast::flat_buffer buffer;
 
+    std::weak_ptr<CallController> controller;
+
     static constexpr size_t queueSize = 8;
-    std::vector<http::message_generator> responceQueue;
+    std::deque<http::message_generator> responceQueue;
 
     std::optional<http::request_parser<http::string_body>> parser;
 };
--- app/src/server/HttpSession.cpp
+++ app/src/server/HttpSession.cpp
@@ -1,19 +1,20 @@
 #include "server/HttpSession.h"
+#include "server/CallController.h"
 
 #include <boost/asio/dispatch.hpp>
 
-HttpSession::HttpSession(tcp::socket&& socket)
-    : stream(std::move(socket))
+HttpSession::HttpSession(tcp::socket&& socket, std::weak_ptr<CallController> controller)
+    : stream(std::move(socket)),
+      controller(std::move(controller))
 {
-    responceQueue.reserve(queueSize);
 }
 
 void HttpSession::run()
 {
-    net::dispatch(stream.get_executor(), beast::bind_front_handler(&HttpSession::doRead, shared_from_this()));
+    net::dispatch(stream.get_executor(), beast::bind_front_handler(&HttpSession::read, shared_from_this()));
 }
 
-void HttpSession::doRead()
+void HttpSession::read()
 {
     parser.emplace();
     parser->body_limit(20);
@@ -26,8 +27,70 @@ void HttpSession::doRead()
 void HttpSession::onRead(beast::error_code errorCode, size_t bytes_transfered)
 {
     boost::ignore_unused(bytes_transfered);
-    if (errorCode)
+
+    if (errorCode == http::error::end_of_stream)
     {
+        close();
         return;
     }
+    if (errorCode)
+    {
+        // Log
+        return;
+    }
+
+    addResponseToQueue(controller.lock()->handleRequest(parser->release()));
+    if (responceQueue.size() == queueSize) return;
+
+    read();
+}
+
+bool HttpSession::write()
+{
+    const bool wasFull = responceQueue.size() == queueSize;
+    if (responceQueue.empty()) return wasFull;
+
+    http::message_generator msg = std::move(responceQueue.front());
+    const bool keepAlive = msg.keep_alive();
+    responceQueue.pop_front();
+
+    beast::async_write(stream, std::move(msg),
+                       beast::bind_front_handler(&HttpSession::onWrite, shared_from_this(), keepAlive));
+
+    return wasFull;
+}
+
+void HttpSession::onWrite(bool keepAlive, beast::error_code errorCode, size_t bytes_transfered)
+{
+    boost::ignore_unused(bytes_transfered);
+
+    if (errorCode)
+    {
+        // log
+        return;
+    }
+
+    if (!keepAlive)
+    {
+        return close();
+    }
+
+    if (write())
+    {
+        read();
+    }
+}
+
+void HttpSession::addResponseToQueue(http::message_generator response)
+{
+    responceQueue.push_back(std::move(response));
+    if (responceQueue.size() != 1) return;
+
+    write();
+}
+
+void HttpSession::close()
+{
+    beast::error_code errorCode;
+    stream.socket().shutdown(tcp::socket::shutdown_send, errorCode);
 }
