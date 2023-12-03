390696190b8d1a7aa192a95c5b6f8f9b59dd5576
Nomlsbad <iliayurasov@yandex.ru>
2023-12-03 11:54:09 +0300

    Update Types.h

--- app/include/Types.h
+++ app/include/Types.h
@@ -4,14 +4,28 @@
 #include <boost/beast.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 
-using IdType = unsigned long long;
-using Date = boost::posix_time::ptime;
-using TimeDuration = boost::posix_time::time_duration;
+#include <nlohmann/json_fwd.hpp>
 
+using IdType = unsigned long long;
+
+
+//------------Boost network-------------//
 using tcp = boost::asio::ip::tcp;
 
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace net = boost::asio;
+//------------Boost network-------------//
+
+
+//--------------Boost time--------------//
+using Date = boost::posix_time::ptime;
+using TimeDuration = boost::posix_time::time_duration;
+//--------------Boost time--------------//
+
+
+//------------Nlohmann JSON------------//
+using json = nlohmann::json;
+//------------Nlohmann JSON------------//
 
 #endif //TYPES_H
