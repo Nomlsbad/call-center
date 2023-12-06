f6247e2ee2237974c0cea3416c45bc2863e1fcd5
Nomlsbad <iliayurasov@yandex.ru>
2023-12-06 05:20:11 +0300

    Add logging to user class

--- app/src/models/User.cpp
+++ app/src/models/User.cpp
@@ -1,14 +1,19 @@
-#include "CallCenter.h"
 #include "models/User.h"
+#include "CallCenter.h"
 #include "config/Configuration.h"
 
+#include <boost/date_time/posix_time/time_formatters.hpp>
 #include <boost/date_time/posix_time/time_parsers.hpp>
 #include <thread>
+
+using boost::posix_time::to_simple_string;
+using boost::posix_time::microsec_clock;
 
 User::User(IdType callId, std::string phone, std::weak_ptr<CallCenter> callCenter)
     : callId(callId),
       phone(std::move(phone)),
-      callCenter(std::move(callCenter))
+      callCenter(std::move(callCenter)),
+      userLogger(Log::Logger::getInstance("CallHandlingLogger"))
 {
     using boost::posix_time::duration_from_string;
 
@@ -23,41 +28,77 @@ User::User(IdType callId, std::string phone, std::weak_ptr<CallCenter> callCente
 
 void User::wait() const
 {
+    LOG4CPLUS_INFO(userLogger, "User: user[" << callId << "] will waiting " << to_simple_string(waitingTime) << "ms.");
+
     auto self = this;
     std::thread waiting(
         [self]
         {
-            if (!self) return;
+            Log::Logger waitingLogger(Log::Logger::getInstance("CallHandlingLogger"));
+            if (!self)
+            {
+                LOG4CPLUS_ERROR(waitingLogger, "User: user was destroyed");
+                return;
+            }
 
             const std::shared_ptr callCenter = self->callCenter.lock();
             const IdType callId = self->getCallId();
             const std::chrono::milliseconds waitingTime(self->waitingTime.total_milliseconds());
 
             std::this_thread::sleep_for(waitingTime);
-            if (!self || self->wasResponded || !callCenter) return;
+            if (!self || !callCenter || self->wasResponded)
+            {
+                if (!self)
+                {
+                    LOG4CPLUS_ERROR(waitingLogger, "User: user was destroyed");
+                }
+                if (!callCenter)
+                {
+                    LOG4CPLUS_ERROR(waitingLogger, "User: call center was destroyed");
+                }
+                return;
+            }
 
-            callCenter->endCall(callId, CallEndingStatus::TIMEOUT, boost::posix_time::microsec_clock::local_time());
+            callCenter->endCall(callId, CallEndingStatus::TIMEOUT, microsec_clock::local_time());
         });
     waiting.detach();
 }
 
 void User::talk() const
 {
+    LOG4CPLUS_INFO(userLogger, "User: user[" << callId << "] will talking " << to_simple_string(waitingTime) << "ms.");
     auto self = this;
     std::thread talking(
         [self]
         {
-            if (!self) return;
+            Log::Logger talkingLogger(Log::Logger::getInstance("CallHandlingLogger"));
+
+            if (!self)
+            {
+                LOG4CPLUS_ERROR(talkingLogger, "User: user was destroyed");
+                return;
+            }
             const std::shared_ptr callCenter = self->callCenter.lock();
             const IdType callId = self->getCallId();
             const std::chrono::milliseconds talkingTime(self->talkingTime.total_milliseconds());
 
             std::this_thread::sleep_for(talkingTime);
-            if (!callCenter) return;
+            if (!callCenter)
+            {
+                LOG4CPLUS_ERROR(talkingLogger, "User: call center was destroyed");
+                return;
+            }
 
-            callCenter->endCall(callId, CallEndingStatus::OK, boost::posix_time::microsec_clock::local_time());
+            callCenter->endCall(callId, CallEndingStatus::OK, microsec_clock::local_time());
         });
     talking.detach();
+}
+
+void User::response()
+{
+    LOG4CPLUS_INFO(userLogger, "User: user[" << callId << "] get response");
+    wasResponded = true;
+    talk();
 }
 
 TimeDuration User::getRandomDuration(const TimeDuration& min, const TimeDuration& max)
@@ -77,10 +118,4 @@ IdType User::getCallId() const
 std::string User::getPhone() const
 {
     return phone;
-}
-
-void User::response()
-{
-    wasResponded = true;
-    talk();
 }
