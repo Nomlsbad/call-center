ce1159dffeedcf2c72c076c41c96e2c3dcbe8100
Yurasov Ilia <iliayurasov@yandex.ru>
2023-12-07 04:08:32 +0300

    New config class (#28)
    * Add interface for config classes
    
    * Add class for configuration from json files
    
    * Replace config classes with new once
    
    * Add build variable with source path

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,12 +1,19 @@
 #include "server/Server.h"
 
+#include <config/Configuration.h>
 #include <log4cplus/configurator.h>
 #include <log4cplus/initializer.h>
 
 int main()
 {
+    const std::string sourceDir = std::string(PROJECT_SOURCE_DIR);
+
+    const std::string configPath = sourceDir + "/config.json";
+    Configuration::initialize(configPath);
+
+    const std::string loggerPropertiesPath = sourceDir + "/log4cplus.properties";
     Log::Initializer initializer;
-    Log::PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("../../log4cplus.properties"));
+    Log::PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT(loggerPropertiesPath));
 
     Server server{};
     server.run();
